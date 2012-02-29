7fc8f3d2ad37c799209bd4ce24400a716cb678dd
Ruslan Kornev <oganer@gmail.com>
2012-02-29 18:48:31 +0100

    Вернулся к эскперименту с картой сайта

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,6 +6,7 @@ SitemapGenerator::Sitemap.create do
     add '/searches', :changefreq => 'daily'
     add '/searches/MN158596/MITSUBISHI/1', :changefreq => 'weekly'
     add '/searches/2330021010/TOYOTA/1', :changefreq => 'weekly'
+    add '/searches/9091902230/TOYOTA/1', :changefreq => 'weekly'
     add '/about', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # called for you when you use the rake task
