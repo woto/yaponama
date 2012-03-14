0f6daa9e4df47a74c33fe4f44ac9958b94ea42a7
Ruslan Kornev <oganer@gmail.com>
2012-03-14 09:54:40 +0100

    Черновые наброски получения изображения на странице info и отдачи статического контента.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,11 @@
 class InfoController < ApplicationController
   def index
+      url = URI.parse("http://37.110.44.140/info/#{params[:catalog_number]}/#{CGI::escape(params[:manufacturer] || '')}")
+      begin
+        resp = Net::HTTP.get_response(url)
+      rescue Exception => e
+        @show_sidebar = true
+        render :status => 404 and return
+      end
   end
 end
