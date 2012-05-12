fcd867636bfd08fe5270ecc1816137bf4b108f27
Kornev Ruslan <oganer@gmail.com>
2012-05-12 20:26:36 +0200

    Наконец сделал выход из очистки. Попутно разбираюсь в прелестях асинхронного программирования :\

--- system/clear.js
+++ system/clear.js
@@ -1,3 +1,28 @@
+function asyncForEach(array, iterator, then) {
+  function loop(i) {
+    if (i < array.length) {
+      iterator(array[i], function() {
+        loop(i + 1);
+      })
+    }
+    else {
+      then();
+    }
+  }
+  loop(0);
+}
+
 redis = require('redis');
 client = redis.createClient();
-client.keys("*", function(one, two){ client.del(two, function(one, two){})})
+
+client.keys("*", function(keys_err, keys){ 
+  if (keys_err) {callback(keys_err); return; }
+  asyncForEach(keys, function(key, next) {
+    client.del(key, function(del_err, two){
+      if (del_err) {callback(del_err); return; }
+      next();
+    });
+  }, function() {
+    process.exit();
+  });
+});
