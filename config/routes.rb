4e67e8108b325a3bbab280eb82ba0622f39f953b
Kornev Ruslan <oganer@gmail.com>
2012-05-28 19:37:51 +0200

    Сделал routes общим для kaprika.ru и www.yaponama.ru, дописал README по мелочи.

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,13 @@ Yaponama::Application.routes.draw do
   #  match '/*path', :to => redirect {|params| "http://www.yaponama.ru/#{params[:path]}"}
   #end
   
-  match '(/(*path))' => redirect{ |params| "http://www.yaponama.ru/#{params[:path].gsub(' ', '%20')}"}, :constraints => lambda { |req| !req.host.include?("www.yaponama.ru") && !req.host.include?("localhost") && !(req.host =~ /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/) }, :defaults => { :path => '' }
+  require "socket"
+  case Socket.gethostname
+  when 'yaponama.ru'
+    match '(/(*path))' => redirect{ |params| "http://www.yaponama.ru/#{params[:path].gsub(' ', '%20')}"}, :constraints => lambda { |req| !req.host.include?("www.yaponama.ru") && !req.host.include?("localhost") && !(req.host =~ /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/) }, :defaults => { :path => '' }
+  when 'kaprika.ru'
+    match '(/(*path))' => redirect{ |params| "http://kaprika.ru/#{params[:path].gsub(' ', '%20')}"}, :constraints => lambda { |req| req.subdomain != '' && !req.host.include?("localhost") && !(req.host =~ /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/) }, :defaults => { :path => '' }
+  end
 
   resources :orders do
     get 'page/:page', :action => :index, :on => :collection
