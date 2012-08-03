f6f652c1e730f44a4c08fd1686042192ad29a789
Руслан Корнев <oganer@gmail.com>
2012-08-03 13:56:05 +0400

    Тоже опережающий коммит. Изменения в BRANDS. Избавился от избыточного :order. :file у всех стал по-умолчанию через sanitize_filename. Сделал идею с :ref PEUGEOT/CITROEN => PEUGEOT, CITROEN. И вообще сделал таким образом, что сначала берется хеш с настройками по-умолчанию, а потом перегружается если такие ключи имеются у конкретного бренда

--- lib/brands.rb
+++ lib/brands.rb
@@ -1,63 +1,93 @@
-class Brands
+module Brands
 
-  def self.sanitize_filename(filename)
-    name = filename.to_s.mb_chars.strip
-    # NOTE: File.basename doesn't work right with Windows paths on Unix
-    # get only the filename, not the whole path
-    # Strip out the non-ascii character
-    name.gsub!(/[^а-яА-Я0-9A-Za-z.\-]/, '_')
+  class BrandHash < ActiveSupport::OrderedHash
+    def defaults(key)
+      { 
+        :title => key, 
+        :catalog => false, 
+        :rating => 3000, 
+        :brand => false, 
+        :file => sanitize_filename(key.downcase) 
+      }
+    end
+
+    def []=(key, value)
+      super key, defaults(key).merge(value)
+    end
+
+    def [](key)
+      if self.key? key
+        super key
+      else
+        defaults(key)
+      end
+    end
+
+
+    def sanitize_filename(filename)
+      name = filename.to_s.mb_chars.strip
+      name.gsub!(/[^а-яА-Я0-9A-Za-z.\-]/, '_')
+    end
   end
 
-  BRANDS = ActiveSupport::OrderedHash.new{|h, k| {
-    :file => sanitize_filename(k).downcase,
-    :title => sanitize_filename(k).upcase,
-    :rating => 3,
-    :brand => false,
-  }}
+  BRANDS = BrandHash[
+    "ALFA ROMEO",         {:rating => 5000, :brand => true,  :catalog => true },
+    "AUDI",               {:rating => 5000, :brand => true,  :catalog => true },
+    "BMW",                {:rating => 5000, :brand => true,  :catalog => true },
+    "CHERY",              {:rating => 5000, :brand => true,  :catalog => true },
+    "CHEVROLET",          {:rating => 5000, :brand => true,  :catalog => true },
+    "CHRYSLER",           {:rating => 5505, :brand => true,  :catalog => true },
+    "CITROEN",            {:rating => 5250, :brand => true,  :catalog => true },
+    "DAEWOO",             {:rating => 5000, :brand => true,  :catalog => true },
+    "DAIHATSU",           {:rating => 5000, :brand => true,  :catalog => true },
+    "DODGE",              {:rating => 5000, :brand => true,  :catalog => true },
+    "FIAT",               {:rating => 5000, :brand => true,  :catalog => true },
+    "FORD",               {:rating => 5000, :brand => true,  :catalog => true },
+    "HONDA",              {:rating => 5000, :brand => true,  :catalog => true },
+    "HYUNDAI",            {:rating => 5000, :brand => true,  :catalog => true },
+    "HYUNDAI / KIA",      {:rating => 5000, :brand => true,  :catalog => false, :ref => ["HYUNDAI", "KIA", "MOBIS"]},
+    "HYUNDAI/KIA",        {:rating => 5000, :brand => true,  :catalog => false, :ref => ["HYUNDAI", "KIA", "MOBIS"]},
+    "INFINITI",           {:rating => 5000, :brand => true,  :catalog => true },
+    "ISUZU",              {:rating => 5000, :brand => true,  :catalog => true },
+    "JAGUAR",             {:rating => 5000, :brand => true,  :catalog => true },
+    "JEEP",               {:rating => 5000, :brand => true,  :catalog => true },
+    "KIA",                {:rating => 5000, :brand => true,  :catalog => true },
+    "LAND ROVER",         {:rating => 5000, :brand => true,  :catalog => true, :title => "LAND&#560;ROVER"},
+    "LEXUS",              {:rating => 5000, :brand => true,  :catalog => true },
+    "MAZDA",              {:rating => 5000, :brand => true,  :catalog => true },
+    "MERCEDES-BENZ",      {:rating => 5000, :brand => true,  :catalog => true, :title => "MERCEDES&#8509;BENZ"},
+    "MINI",               {:rating => 5000, :brand => true,  :catalog => true },
+    "MITSUBISHI",         {:rating => 5510, :brand => true,  :catalog => true },
+    "NISSAN",             {:rating => 5000, :brand => true,  :catalog => true },
+    "OPEL",               {:rating => 5000, :brand => true,  :catalog => true },
+    "PEUGEOT",            {:rating => 5500, :brand => true,  :catalog => true },
+    "PEUGEOT / CITROEN",  {:rating => 5400, :brand => true,  :catalog => false, :ref => ["PEUGEOT", "CITROEN"] },
+    "PEUGEOT/CITROEN",    {:rating => 5400, :brand => true,  :catalog => false, :ref => ["PEUGEOT", "CITROEN"] },
+    "RENAULT",            {:rating => 5000, :brand => true,  :catalog => true },
+    "SAAB",               {:rating => 5000, :brand => true,  :catalog => true },
+    "SEAT",               {:rating => 5000, :brand => true,  :catalog => true },
+    "SKODA",              {:rating => 5000, :brand => true,  :catalog => true },
+    "SSANGYONG",          {:rating => 5000, :brand => true,  :catalog => true },
+    "SUBARU",             {:rating => 5000, :brand => true,  :catalog => true },
+    "SUZUKI",             {:rating => 5000, :brand => true,  :catalog => true },
+    "TOYOTA",             {:rating => 5500, :brand => true,  :catalog => true },
+    "VOLKSWAGEN",         {:rating => 5000, :brand => true,  :catalog => true },
+    "VOLVO",              {:rating => 5000, :brand => true,  :catalog => true },
+    "AKEBONO",            {:rating => 4000, :brand => false, :catalog => false },
+    "NIBK",               {:rating => 4000, :brand => false, :catalog => false },
+    "ALLIED NIPPON",      {:rating => 3000, :brand => false, :catalog => false },
+    "SIDEM",              {:rating => 3000, :brand => false, :catalog => false },
+    "FEBEST",             {:rating => 3000, :brand => false, :catalog => false },
+    "NIPPARTS",           {:rating => 1000, :brand => false, :catalog => false },
+    "JAPAN CARS",         {:rating => 1000, :brand => false, :catalog => false },
+    "CHAMPION",           {:rating => 1000, :brand => false, :catalog => false },
+    "MOBIS",              {:rating => 5000, :brand => true,  :catalog => false },
+    "GENERAL MOTORS",     {:rating => 5000, :brand => true,  :catalog => false, :ref => ["CADILLAC", "CHEVROLET", "OPEL"] }, 
+    "KYB",                {:rating => 4000, :brand => false, :catalog => false}  
+  ]
 
-  BRANDS.merge!(ActiveSupport::OrderedHash[
-    "ALFA ROMEO", { :file => "alfa_romeo", :title => "ALFA ROMEO", :rating => 5, :brand => true },
-    "AUDI", { :file => "audi", :title => "AUDI", :rating => 5, :brand => true },
-    "BMW", { :file => "bmw", :title => "BMW", :rating => 5, :brand => true },
-    "CHERY", { :file => "chery", :title => "CHERY", :rating => 5, :brand => true },
-    "CHEVROLET", { :file => "chevrolet", :title => "CHEVROLET", :rating => 5, :brand => true },
-    "CHRYSLER", { :file => "chrysler", :title => "CHRYSLER", :rating => 5, :brand => true },
-    "CITROEN", { :file => "citroen", :title => "CITROEN", :rating => 5, :brand => true },
-    "DAEWOO", { :file => "daewoo", :title => "DAEWOO", :rating => 5, :brand => true },
-    "DAIHATSU", { :file => "daihatsu", :title => "DAIHATSU", :rating => 5, :brand => true },
-    "DODGE", { :file => "dodge", :title => "DODGE", :rating => 5, :brand => true },
-    "FIAT", { :file => "fiat", :title => "FIAT", :rating => 5, :brand => true },
-    "FORD", { :file => "ford", :title => "FORD", :rating => 5, :brand => true },
-    "HONDA", { :file => "honda", :title => "HONDA", :rating => 5, :brand => true },
-    "HYUNDAI", { :file => "hyundai", :title => "HYUNDAI", :rating => 5, :brand => true },
-    "INFINITI", { :file => "infiniti", :title => "INFINITI", :rating => 5, :brand => true },
-    "ISUZU", { :file => "isuzu", :title => "ISUZU", :rating => 5, :brand => true },
-    "JAGUAR", { :file => "jaguar", :title => "JAGUAR", :rating => 5, :brand => true },
-    "JEEP", { :file => "jeep", :title => "JEEP", :rating => 5, :brand => true },
-    "KIA", { :file => "kia", :title => "KIA", :rating => 5, :brand => true },
-    "LAND ROVER", { :file => "land_rover", :title => "LAND&#160;ROVER", :rating => 5, :brand => true },
-    "LEXUS", { :file => "lexus", :title => "LEXUS", :rating => 5, :brand => true },
-    "MAZDA", { :file => "mazda", :title => "MAZDA", :rating => 5, :brand => true },
-    "MERCEDES-BENZ", { :file => "mercedes_benz", :title => "MERCEDES&#8209;BENZ", :rating => 5, :brand => true },
-    "MINI", { :file => "mini", :title => "MINI", :rating => 5, :brand => true },
-    "MITSUBISHI", { :file => "mitsubishi", :title => "MITSUBISHI", :rating => 5, :brand => true },
-    "NISSAN", { :file => "nissan", :title => "NISSAN", :rating => 5, :brand => true },
-    "OPEL", { :file => "opel", :title => "OPEL", :rating => 5, :brand => true },
-    "PEUGEOT", { :file => "peugeot", :title => "PEUGEOT", :rating => 5, :brand => true },
-    "RENAULT", { :file => "renault", :title => "RENAULT", :rating => 5, :brand => true },
-    "SAAB", { :file => "saab", :title => "SAAB", :rating => 5, :brand => true },
-    "SEAT", { :file => "seat", :title => "SEAT", :rating => 5, :brand => true },
-    "SKODA", { :file => "skoda", :title => "SKODA", :rating => 5, :brand => true },
-    "SSANGYONG", { :file => "ssang_yong", :title => "SSANGYONG", :rating => 5, :brand => true },
-    "SUBARU", { :file => "subaru", :title => "SUBARU", :rating => 5, :brand => true },
-    "SUZUKI", { :file => "suzuki", :title => "SUZUKI", :rating => 5, :brand => true },
-    "TOYOTA", { :file => "toyota", :title => "TOYOTA", :rating => 5, :brand => true },
-    "VOLKSWAGEN", { :file => "volkswagen", :title => "VOLKSWAGEN", :rating => 5, :brand => true },
-    "VOLVO", { :file => "volvo", :title => "VOLVO", :rating => 5, :brand => true },
-    "FEBEST", { :file => "febest", :title => "febest", :rating => 4, :brand => false },
-  ])
-
-  def self.is_brand? brand
-    BRANDS.include? brand
-  end
+    # CHECK не знаю где это используется, вроде не надо
+    #def self.is_brand? brand
+    #  BRANDS.include? brand
+    #end
 end
