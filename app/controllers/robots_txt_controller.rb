937e01864795fa8dcd0759c754706d03423067b4
Kornev Ruslan <oganer@gmail.com>
2012-08-19 10:03:03 +0200

    Уменьшил таймаут Crawl-Delay с 10 до 5 сек., а так же добавил ip seomoz'a в список кешируемых

--- app/controllers/robots_txt_controller.rb
+++ app/controllers/robots_txt_controller.rb
@@ -4,7 +4,7 @@ class RobotsTxtController < ApplicationController
 User-Agent: *
 Allow: /
 Disallow: /info/
-Crawl-delay: 10
+Crawl-delay: 5
 
 Sitemap: http://#{APP_CONFIG['site_address']}/sitemap_index.xml.gz
 Sitemap: http://#{APP_CONFIG['site_address']}/sitemap1.xml.gz
