1f9fd819cb8a27a6703e23e3c1773111fcc755c6
Руслан Корнев <oganer@gmail.com>
2012-09-15 04:14:04 +0400

    Убрал Disallow правила из robots.txt х.з. может из-за этого как-то

--- app/controllers/robots_txt_controller.rb
+++ app/controllers/robots_txt_controller.rb
@@ -3,9 +3,6 @@ class RobotsTxtController < ApplicationController
     render :text => <<EOF
 User-Agent: *
 Allow: /
-Disallow: /info/
-Disallow: /*?skip_mobile=true
-Disallow: /*?skip_mobile=false
 
 Sitemap: http://#{APP_CONFIG['site_address']}/sitemap_index.xml.gz
 
