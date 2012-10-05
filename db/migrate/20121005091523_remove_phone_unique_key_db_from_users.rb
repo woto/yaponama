750f60dd49d1968c79037045224cc66a4a3e11b9
Руслан Корнев <oganer@gmail.com>
2012-10-05 13:17:30 +0400

    Удалил unique key из таблицы users с столбца phone для того чтобы можно было разрешать пустые номера телефонов

---
+++ db/migrate/20121005091523_remove_phone_unique_key_db_from_users.rb
@@ -0,0 +1,9 @@
+class RemovePhoneUniqueKeyDbFromUsers < ActiveRecord::Migration
+  def up
+    remove_index :users, :phone
+  end
+
+  def down
+    add_index :users, :phone, :unique => true
+  end
+end
